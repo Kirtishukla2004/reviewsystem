2a1fa49a99da0e4b1453ee4839d406a8d69a4cda
Kirti Shukla <shuklakirti2004@gmail.com>
2026-03-26 00:43:40 +0530

    added other link

--- reviewsystembackend/app/main.py
+++ reviewsystembackend/app/main.py
@@ -16,6 +16,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 origins = [
     "https://reviewvibe-app.vercel.app",
     "https://reviewvibe-app.vercel.app",
+    "https://reviewvibe.vercel.app"
 ]
 
 app.add_middleware(
